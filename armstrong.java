c65377235cd166718d95df71ef62760703028739
nikkhil-ss <nikhil.singh_cs19@gla.ac.in>
2023-04-27 19:59:16 +0530

    Armstrong Initial Uplaod

---
+++ armstrong.java
@@ -0,0 +1,25 @@
+class armstrong {
+    public boolean check(int num) {
+        int sum = 0;
+        int temp = num;
+        while(temp > 0) {
+            int n = temp % 10;
+            sum += (n*n*n);
+            temp=temp/10;
+        }
+        if(sum == num)
+            return true;
+        else
+            return false;
+    }
+
+
+    public static void main(String[] args) {
+        armstrong obj = new armstrong();
+        int num = 371;
+        if(obj.check(num))
+            System.out.println(num+" is an Armstrong number.");
+        else
+            System.out.println(num+" is not an Armstrong number.");
+    }
+}
